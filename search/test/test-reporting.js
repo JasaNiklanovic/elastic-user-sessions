a10401b07aa8b077c1357acac7e4df14e0fd84ac
Jasa Niklanovic <jasa.niklanovic@gmail.com>
2018-05-31 19:43:27 +0200

    Dev 2

--- search/test/test-reporting.js
+++ search/test/test-reporting.js
@@ -1,7 +1,7 @@
 var expect  = require('chai').expect;
 let Reporting = require('../reporting.js')
 
-it('should return an array of user sessions data', async () => {
+it('should return a collection of user sessions data', async () => {
   const result = await new Reporting().process([1355, 315, 1535, 5135, 333, 1, 8824, 9999, 1000000, 666])
   expect(result).to.be.an('array')
   expect(result[0]).to.have.property('name')
