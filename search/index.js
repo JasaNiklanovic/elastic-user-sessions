7cbe83770250166b5571e1ecc3498c618542b955
Jasa Niklanovic <jasa.niklanovic@gmail.com>
2018-05-31 19:40:11 +0200

    Commit 1 dev

--- search/index.js
+++ search/index.js
@@ -4,7 +4,7 @@ const USER_IDS = [1355, 315, 1535, 5135, 333, 1, 8824, 9999, 1000000, 666]
 const index = async () => {
   try {
     const result = await new Reporting().process(USER_IDS)
-    console.log(result, 'result')
+    console.log(result)
   } catch (e) {
     console.log(e)
   }
